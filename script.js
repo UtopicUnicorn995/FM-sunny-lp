e736d938f11dc17f914d7dbd9bd01b94e1a35b74
utopicUnicorn <christianbarrios995@gmail.com>
2023-11-09 18:54:16 +0800

    FM-sunny-lp: added media queries for FM challenge

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
       $('.hamburger-lines').toggleClass('active')
       $('.nav-list').fadeToggle(500)
   })
+
+  var win = $(this)
+  if (win.width() >= 786) { 
+    $('.nav-list').show()
+  }
+
+  $(window).on('resize', function(){
+    if (win.width() >= 786) { 
+      $('.nav-list').show()
+    }else{
+      $('.nav-list').hide()
+    }
+});
 })
